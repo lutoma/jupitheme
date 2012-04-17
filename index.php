0bd43a993ceb0a00aa9fce6745b593203763aa9d
Lukas Martini <lutoma@ohai.su>
2012-04-17 17:36:10 +0200

    Making the post title a link

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <div role="main">
       <?php while (have_posts()) : the_post() ?>
         <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-          <h2><?php the_title(); ?></h2>
+          <h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
           <?php the_content(); ?>
           <?php comments_popup_link(__('Leave a comment', 'your-theme'), __('1 Comment', 'your-theme'), _('% Comments', 'your-theme'), 'comment-button btn') ?>
         </div>
