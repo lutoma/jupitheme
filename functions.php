89bb035aaaf4f2501a61d19380b33737c46139d1
Lukas Martini <lutoma@ohai.su>
2012-04-18 17:56:42 +0200

    Making sidebar dynamic

--- functions.php
+++ functions.php
@@ -1,12 +1,21 @@
 <?php
 // Register the menus
-add_action( 'init', 'register_menus' );
+add_action('init', 'register_menus');
  
 function register_menus() {
 	register_nav_menus(
 		array(
-			'primary' => __( 'Primary Menu' ),
+			'primary' => __('Primary Menu'),
 		)
 	);
 }
+
+// Sidebar
+if (function_exists('register_sidebar'))
+	register_sidebar(array(
+		'before_widget' => '<div class="box">',
+		'after_widget' => '</div>',
+		'before_title' => '<h2>',
+		'after_title' => '</h2>',
+	));
 ?>
--- index.php
+++ index.php
@@ -1,14 +1,7 @@
 <?php get_header(); ?>
   <div id="wrapper">
     <aside>
-      <h2>JuPis auf…</h2>
-      <div class="box">
-        <a href="/feed/"><img src="//www.junge-piraten.de/wp-content/uploads/2010/02/48px-Feed-icon.svg_-e1281671741681.png" style="border-width: 0px;" alt="RSS-/Atom-Feed-Icon" /></a><a href="//twitter.com/jungePiraten"><img src="https://www.junge-piraten.de/wp-content/uploads/2012/02/t_logo-a.png" style="margin-left: 10px; border-width: 0px;" alt="Twitter-Icon" /></a>
-      </div>
-      <h2>Piraten anderswo</h2>
-      <div class="box">
-        Derp herp
-      </div>
+      <?php dynamic_sidebar(); ?> 
     </aside>
 
     <div role="main">
@@ -16,7 +9,7 @@
         <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
           <h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
           <?php the_content(); ?>
-          <?php comments_popup_link(__('Leave a comment', 'your-theme'), __('1 Comment', 'your-theme'), _('% Comments', 'your-theme'), 'comment-button btn') ?>
+          <?php if(!is_single() && !is_page()) comments_popup_link(__('Leave a comment', 'your-theme'), __('1 Comment', 'your-theme'), _('% Comments', 'your-theme'), 'comment-button btn'); ?>
         </div>
       <?php endwhile; ?>
 
