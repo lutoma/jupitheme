e514d2116bb3168b204c7d6f1c29f0a617213147
Lukas Martini <lutoma@ohai.su>
2012-04-19 19:27:13 +0200

    Making the whole thing valid HTML5 again

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 <!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 <head>
   <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <!--[if ie]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><![endif]-->
   <title><?php wp_title('&laquo;', true, 'right'); bloginfo('name'); ?></title>
   <meta name="description" content="Die Jungen Piraten sind die Jugendorganisation der Piratenpartei und setzen sich für die Belange Jugendlicher in der Politik ein.">
 
@@ -21,7 +21,7 @@
 
   <?php wp_head(); ?>
 
-  <link rel="a//lternate" type="application/rss+xml" href="<?php bloginfo('rss2_url'); ?>" title="<?php printf( __( '%s latest posts', 'your-theme' ), wp_specialchars( get_bloginfo('name'), 1 ) ); ?>" />
+  <link rel="alternate" type="application/rss+xml" href="<?php bloginfo('rss2_url'); ?>" title="<?php printf( __( '%s latest posts', 'your-theme' ), wp_specialchars( get_bloginfo('name'), 1 ) ); ?>" />
   <link rel="alternate" type="application/rss+xml" href="<?php bloginfo('comments_rss2_url') ?>" title="<?php printf( __( '%s latest comments', 'your-theme' ), wp_specialchars( get_bloginfo('name'), 1 ) ); ?>" />
   <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 </head>
@@ -30,26 +30,15 @@
   <div id="headerwrapper">
   <header>
     <a id="name" href="<?php echo site_url("/"); ?>" title="<?php bloginfo( 'name' ) ?>" rel="home"><span class="light">Junge</span><br /> PIRATEN</a>
-    </p>
     <img id="signet" src="<?php bloginfo('template_directory'); ?>/img/signet.png" />
 
-    <nav>
     <!--
-      <ul>
         <li class="home"><a href="/"><img src="<?php bloginfo('template_directory'); ?>/img/home_16x16.png" alt="Hauptseite" width="16" height="16" /></a></li>
-        <li><a href="/wer-wir-sind">Wer wir sind</a></li>
-        <li>Aktiv werden</li>
-        <li>Presse</li>
-        <li>Wiki</li>
-        <li>Forum</li>
-        <li class="highlight">Spenden</li>
-
-      </ul>
-    </nav>-->
+    -->
     <?php function menu_empty() {}; wp_nav_menu(array('container' => 'nav', 'container_class' => '', 'theme_location' => 'primary', 'fallback_cb' => menu_empty, 'link_before' => '')); ?>
   </header>
   </div>
-  <div role="slider">
+  <div id="slider">
     <div class="box">
       <span class="plus">+</span>
       <h1><span>WER WIR SIND</span></h1>
