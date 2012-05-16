04ab7f38ea112b3bfacfcab0d1dd7f8e4d81c3bc
Lukas Martini <lutoma@ohai.su>
2012-05-16 21:40:41 +0200

    Display post media information & minor changes

--- index.php
+++ index.php
@@ -12,6 +12,11 @@
       <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
         <h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
         <div class="content">
+          <?php if(!is_page()): ?>
+            <div class="meta">
+              <?php the_author(); ?> – Allgemeines – <?php the_date('d. m. Y'); ?>
+            </div>
+          <?php endif; ?>
           <?php the_content(); ?>
         
           <?php if(!is_single() && !is_page()) comments_popup_link(__('Leave a comment', 'your-theme'), __('1 Comment', 'your-theme'), _('% Comments', 'your-theme'), 'comment-button btn'); ?>
