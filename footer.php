d168ae95e947f1cb32007a66f4a0cf975727cc0e
root <root@magrathea.lutoma.org>
2012-07-15 09:22:04 +0200

    Small design changes

--- footer.php
+++ footer.php
@@ -9,8 +9,7 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
   <script>window.jQuery || document.write('<script src="<?php bloginfo('template_directory'); ?>/js/libs/jquery-1.7.1.min.js"><\/script>')</script>
 
-  <script src="<?php bloginfo('template_directory'); ?>/js/plugins.js"></script>
-  <script src="<?php bloginfo('template_directory'); ?>/js/script.js"></script>
+  <script src="<?php bloginfo('template_directory'); ?>/js/menu.js"></script>
 </body>
 </html>
 
