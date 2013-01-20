aa865a9b7e51954eeb56403a91702662f3aae583
Lukas Martini <hello@lutoma.org>
2013-01-20 05:08:24 +0100

    Image above title

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
     <?php while (have_posts()) : the_post() ?>
     <article>
       <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-        <h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 	<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
+        <h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
         <div class="content">
           <?php if(!is_page()): ?>
             <div class="meta">
