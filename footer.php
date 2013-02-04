e6c2148f14f832073b0b7bc32c65bf5640f203c2
Leonard Techel <mail@telelo.de>
2013-02-04 15:49:46 +0100

    js: removed all the unneccessary javascript \o/

--- footer.php
+++ footer.php
@@ -4,9 +4,6 @@
 		</footer>
 
 		<?php wp_footer(); ?>
-
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-		<script>window.jQuery || document.write('<script src="<?php bloginfo('template_directory'); ?>/js/libs/jquery-1.7.1.min.js"><\/script>')</script>
 	</body>
 </html>
 
